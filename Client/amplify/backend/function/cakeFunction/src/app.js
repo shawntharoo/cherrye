17314d725efba42f48ef3088adc13c088a5ca6e1
shawntharoo <tharooshawn@gmail.com>
2021-04-30 11:50:13 +0930

    new backend env with only cherryapi and cakefunction

--- Client/amplify/backend/function/cakeFunction/src/app.js
+++ Client/amplify/backend/function/cakeFunction/src/app.js
@@ -1,10 +1,18 @@
-/*
+/* Amplify Params - DO NOT EDIT
+	API_CHERRYEAPI_APIID
+	API_CHERRYEAPI_APINAME
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT *//*
 Copyright 2017 - 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
     http://aws.amazon.com/apache2.0/
 or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and limitations under the License.
 */
+
+
+
 
 var express = require('express')
 var bodyParser = require('body-parser')
@@ -114,6 +122,7 @@ app.get('/cakes', function(req, res) {
 // });
 
 //Comment section end *************************************************************************************************
+
 
 app.listen(3000, function() {
     console.log("App started")
